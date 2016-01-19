ba7f0881fee1e18470b60bbe8ae26a13f1ac5b12
Andrés Esteban <aesteban@grupocmc.es>
2016-01-19 18:44:51 +0100

    Update MainServlet.java

--- tomcat-jdbc/src/main/java/org/openshift/quickstarts/tomcat/servlet/MainServlet.java
+++ tomcat-jdbc/src/main/java/org/openshift/quickstarts/tomcat/servlet/MainServlet.java
@@ -49,9 +49,9 @@ public class MainServlet extends HttpServlet {
                     //    );
                     //}
                     
-                    sb.append("MYSQL_DATABASE:" + System.getEnv("MYSQL_DATABASE") + "<br/>" );
-                    sb.append("MYSQL_PASSWORD:" + System.getEnv("MYSQL_PASSWORD") + "<br/>");
-                    sb.append("MYSQL_USER:" + System.getEnv("MYSQL_USER") + "<br/>");
+                    sb.append("MYSQL_DATABASE:" + System.getenv("MYSQL_DATABASE") + "<br/>" );
+                    sb.append("MYSQL_PASSWORD:" + System.getenv("MYSQL_PASSWORD") + "<br/>");
+                    sb.append("MYSQL_USER:" + System.getenv("MYSQL_USER") + "<br/>");
 
                 } else if (insideLoop) {
                     sb.append(line).append("\n");
